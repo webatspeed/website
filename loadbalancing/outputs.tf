1b328a2518fef6204fffbae5d5a9ce3cfb26b8b6
Torsten Krohn <torsten.krohn@webatspeed.com>
2023-09-27 13:53:45 +0200

    Display node URL and kube config command

--- loadbalancing/outputs.tf
+++ loadbalancing/outputs.tf
@@ -1,3 +1,7 @@
 output "lb_target_group_arn" {
   value = aws_lb_target_group.webatspeed_tg.arn
 }
+
+output "lb_endpoint" {
+  value = aws_lb.webatspeed_lb.dns_name
+}
---
+++ outputs.tf
@@ -0,0 +1,13 @@
+output "load_balancer_endpoint" {
+  value = module.loadbalancing.lb_endpoint
+}
+
+output "instances" {
+  value     = { for i in module.compute.instance : i.tags.Name => "${i.public_ip}:${module.compute.instance_port}" }
+  sensitive = true
+}
+
+output "kube_config" {
+  value     = [for i in module.compute.instance : "export KUBECONFIG=./orchestration/k3s-${i.tags.Name}.yaml"]
+  sensitive = true
+}
