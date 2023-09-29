e00e7b5f14838963a5906e1e732bdab2d00611a6
Torsten Krohn <torsten.krohn@webatspeed.com>
2023-09-29 16:08:24 +0200

    Output address list

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "load_balancer_endpoint" {
-  value = module.loadbalancing.lb_endpoint
-}
-
 output "instances" {
   value     = { for i in module.compute.instance : i.tags.Name => "${i.public_ip}:${module.compute.instance_port}" }
   sensitive = true
@@ -10,4 +6,11 @@ output "instances" {
 output "kube_config" {
   value     = [for i in module.compute.instance : "export KUBECONFIG=./orchestration/config/k3s-${i.tags.Name}.yaml"]
   sensitive = true
+}
+
+output "addresses" {
+  value = concat(
+    [for route in module.routing.routes : "https://${route}"],
+    ["http://${module.loadbalancing.lb_endpoint}"]
+  )
 }
--- routing/output.tf
+++ routing/output.tf
@@ -1,3 +1,7 @@
 output "certificate_arn" {
   value = aws_acm_certificate.webatspeed_acm_cert.arn
 }
+
+output "routes" {
+  value = [for i in aws_route53_record.webatspeed_de : i.name]
+}
