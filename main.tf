4e1182dbf04d0f16cc4b312ff199c7b920b5d734
Torsten Krohn <torsten.krohn@webatspeed.com>
2023-09-29 15:40:24 +0200

    Format root main

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 module "networking" {
-  source   = "./networking"
-  vpc_cidr = local.vpc_cidr
+  source = "./networking"
 
-  max_subnets          = 20
-  public_subnet_count  = 2
-  public_cidrs         = [for i in range(2, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
-  private_subnet_count = 2
-  private_cidrs        = [for i in range(1, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
-  security_groups      = local.security_groups
-
+  vpc_cidr               = local.vpc_cidr
+  max_subnets            = 20
+  public_subnet_count    = 2
+  public_cidrs           = [for i in range(2, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
+  private_subnet_count   = 2
+  private_cidrs          = [for i in range(1, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
+  security_groups        = local.security_groups
   create_db_subnet_group = true
 }
 
 module "loadbalancing" {
-  source         = "./loadbalancing"
+  source = "./loadbalancing"
+
   public_sg      = module.networking.public_sg
   public_subnets = module.networking.public_subnets
 
@@ -30,13 +30,15 @@ module "loadbalancing" {
 }
 
 module "routing" {
-  source   = "./routing"
+  source = "./routing"
+
   dns_name = module.loadbalancing.lb_endpoint
   zone_id  = module.loadbalancing.lb_zone_id
 }
 
 module "database" {
-  source                 = "./database"
+  source = "./database"
+
   db_storage             = 10
   db_engine_version      = "5.7"
   db_instance_class      = "db.t2.micro"
