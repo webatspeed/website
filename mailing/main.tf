41b062862b74c1d68933b69c58ca89475cf57102
Torsten Krohn <torsten.krohn@webatspeed.com>
2023-10-09 12:18:26 +0200

    Add email template Please Confirm

--- mailing/main.tf
+++ mailing/main.tf
@@ -27,3 +27,10 @@ resource "aws_iam_user_policy_attachment" "webatspeed_iam_ses_policy_attachment"
   user       = aws_iam_user.webatspeed_smtp_user.name
   policy_arn = aws_iam_policy.webatspeed_iam_ses_policy.arn
 }
+
+resource "aws_ses_template" "webatspeed_ses_please_confirm" {
+  name    = "please-confirm"
+  subject = "Welcome to Web at Speed - Please confirm your email address"
+  text    = file("${path.module}/templates/please-confirm.txt")
+  html    = file("${path.module}/templates/please-confirm.html")
+}
