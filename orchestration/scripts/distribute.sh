04e0f23f6d66efabf56c4a0ea1b652fa86c32e4a
Torsten Krohn <torsten.krohn@webatspeed.com>
2023-11-12 17:59:53 +0100

    Simplify scripts

--- orchestration/scripts/distribute.sh
+++ orchestration/scripts/distribute.sh
@@ -5,8 +5,7 @@ while true; do
     read -p "Do you wish to distribute the newsletter (y/n)? " yn
     case $yn in
         [Yy]* )
-          POD=$(kubectl get pods -l app=frontend -o jsonpath="{.items[0].metadata.name}");
-          kubectl exec $POD -- wget -q --spider --post-data '' http://subscription-service:8080/v1/subscription/distribute;
+          kubectl exec deployment/frontend -- wget -q --spider --post-data '' http://subscription-service:8080/v1/subscription/distribute;
           break;;
         [Nn]* )
           exit;;
--- orchestration/scripts/findemail.sh
+++ orchestration/scripts/findemail.sh
@@ -10,13 +10,12 @@ while true; do
     read -p "Do you wish to search for $1 (y/n)? " yn
     case $yn in
         [Yy]* )
-          POD=$(kubectl get pods -l app=mongodb -o jsonpath="{.items[0].metadata.name}");
           MONGO_USERNAME=$(kubectl get secret mongo-credentials --template={{.data.username}}|base64 --decode);
           MONGO_PASSWORD=$(kubectl get secret mongo-credentials --template={{.data.password}}|base64 --decode);
           MONGO_QUERY="db.subscription.find({\"email\": \"$1\"});"
           MONGO_CONNECTION="mongodb://$MONGO_USERNAME:$MONGO_PASSWORD@localhost:27017/subscription?authSource=admin"
           MONGO_SEARCH="mongosh --eval '$MONGO_QUERY' $MONGO_CONNECTION";
-          kubectl exec $POD -- sh -c "$MONGO_SEARCH";
+          kubectl exec deployment/mongodb -- sh -c "$MONGO_SEARCH";
           break;;
         [Nn]* )
           exit;;
