1449fe433569c6c407f0bb18601583e86a77c38c
Torsten Krohn <torsten.krohn@webatspeed.com>
2024-02-29 23:04:42 +0100

    Enable EFS backup

--- storage/main.tf
+++ storage/main.tf
@@ -1,6 +1,14 @@
 resource "aws_efs_file_system" "webatspeed_efs" {
 }
 
+resource "aws_efs_backup_policy" "webatspeed_efs_bak" {
+  file_system_id = aws_efs_file_system.webatspeed_efs.id
+
+  backup_policy {
+    status = "ENABLED"
+  }
+}
+
 resource "aws_efs_mount_target" "webatspeed_efs_mount_target" {
   count           = length(var.private_subnet_ids)
   file_system_id  = aws_efs_file_system.webatspeed_efs.id
